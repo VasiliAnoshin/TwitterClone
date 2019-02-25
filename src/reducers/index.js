c7b2c16fa8112acea7b02aa2e0d2dada5bba72e6
Vasili <anoshin45@gmail.com>
2019-02-25 13:51:26 -0800

    add Provider

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,6 @@
 //export our invocation to combine reducers
 
-import {combineReducers} from 'redux'
+import { combineReducers } from 'redux'
 import authedUser from './authedUser'
 import users from './users'
 import tweets from '/tweets'
