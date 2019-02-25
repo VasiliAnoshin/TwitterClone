977e7d1b217a06201fcd664c0db5233f3dd45507
Vasili <anoshin45@gmail.com>
2019-02-25 13:55:50 -0800

    add tweets

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,7 +3,7 @@
 import { combineReducers } from 'redux'
 import authedUser from './authedUser'
 import users from './users'
-import tweets from '/tweets'
+import tweets from './tweets'
 
 export default combineReducers({
     authedUser,
