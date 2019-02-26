b335fd37c56e2cb14e5b5153196a54f3efc5b331
Dev computer <eli@alpha-beta.co.il>
2019-02-26 14:11:15 +0200

    reducer comments

--- src/middleware/logger.js
+++ src/middleware/logger.js
@@ -2,10 +2,10 @@
 const logger = (store) => (next) =>(action)=> {
     console.group(action.type)
         console.log('The action: ', action)
-        const returnValue = next(action)
+        const returnValue = next(action) //call reducer , return next action object and set it to result . 
         console.log('The new state: ', store.getState())
     console.groupEnd()
-    return returnValue
+    return returnValue //Return the next action object to middleware.
 }
 
 export default logger
