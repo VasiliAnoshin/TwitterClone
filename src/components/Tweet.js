19723ba52d231132800e9e430b923a051c0b1df5
Dev computer <eli@alpha-beta.co.il>
2019-02-26 16:52:16 +0200

    format tweet

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -4,7 +4,13 @@ import {formatTweet} from '../utils/helpers'
 
 class Tweet extends Component{
     render(){
+        const { tweet } = this.props
+        if(tweet === null){
+            return <p> This Tweet doesn't existed </p>
+        }
+
         console.log(this.props)
+        
         return(
             <div className ='tweet'>
                 
@@ -18,9 +24,12 @@ class Tweet extends Component{
 //ownProps recieved as {id}
 function mapStateToProps({authedUser,users,tweets}, {id}){
     const tweet = tweets[id]
+    const parentTweet =tweet? tweets[tweet.replyingTo] : null
     return{
         authedUser,
-        tweet: formatTweet(tweet, users[tweet.author],authedUser)
+        tweet: 
+        tweet ? formatTweet(tweet, users[tweet.author],authedUser,parentTweet)
+        : null
     }
 }
 
