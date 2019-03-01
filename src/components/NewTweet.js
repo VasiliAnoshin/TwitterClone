17bf9bbe618aa48eacb533b5534a409e93451c1d
Vasili <anoshin45@gmail.com>
2019-03-01 13:18:26 -0800

    Redirect in submit

--- src/components/NewTweet.js
+++ src/components/NewTweet.js
@@ -1,11 +1,13 @@
 import React , {Component} from 'react'
 import {connect} from 'react-redux'
 import {handleAddTweet} from '../actions/tweets'
+import { Redirect } from 'react-router-dom'
 
 class NewTweet extends Component
 {
     state = {
         text: '',
+        toHome: false,
     }
 
     handleChange = (e) => {
@@ -25,13 +27,17 @@ class NewTweet extends Component
 
          console.log('New Tweet: ', text)    
          this.setState(() => ({
-          text: ''
+          text: '',
+          toHome: id ? false : true,
         }))
       }
 
     render(){
-        const {text} = this.state
+        const {text,toHome} = this.state
         const tweetLeft = 280 - text.length
+        if (toHome === true) {
+            return <Redirect to='/' />
+          }
         return(
             <div>
                 <h3 className = 'center'>Compose New Tweet </h3> 
