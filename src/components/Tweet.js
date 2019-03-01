45ed55163ba5754005f9b93c842c069101328beb
Vasili <anoshin45@gmail.com>
2019-03-01 13:25:25 -0800

    add navigation to tweet

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -5,6 +5,7 @@ import { TiArrowBackOutline } from 'react-icons/ti/index'
 import { TiHeartOutline } from 'react-icons/ti/index'
 import { TiHeartFullOutline } from 'react-icons/ti/index'
 import { handleToggleTweet } from '../actions/tweets'
+import { Link, withRouter } from 'react-router-dom'
 
 class Tweet extends Component{
     
@@ -20,7 +21,7 @@ class Tweet extends Component{
 
     toParent = (e, id) => {
         e.preventDefault()
-        // todo: Redirect to parent Tweet.
+        this.props.history.push(`/tweet/${id}`)
       }
     
       render(){
@@ -34,7 +35,8 @@ class Tweet extends Component{
         
         console.log(this.props)
         return(
-            <div className ='tweet'>
+            
+            <Link to={`/tweet/${id}`} className='tweet'>
                 <img
                 src ={avatar}
                 alt = {`Avatar of ${name}`}
@@ -63,7 +65,7 @@ class Tweet extends Component{
                     <span>{likes !== 0 && likes}</span>
                     </div>
                 </div>
-            </div>
+            </Link>
     )}
 }
 //ownProps (optional)
@@ -82,4 +84,4 @@ function mapStateToProps({authedUser,users,tweets}, {id}){
     }
 }
 
-export default connect(mapStateToProps)(Tweet)
+export default withRouter(connect(mapStateToProps)(Tweet))
